ee89995b64529e1298e4042ac87bfcf3cb3aeca9
Dimitri V <dvardidze15@gmail.com>
2024-08-02 19:34:32 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function showNav() {
+    document.getElementById("hiddennav").style.display = 'block'
+    document.getElementById("creatives").style.display = 'none'
+}
+function hideNav() {
+    document.getElementById("hiddennav").style.display = 'none'
+    document.getElementById("creatives").style.display = 'block'
+}
